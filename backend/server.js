cab42530ad420b14792d643296f23b732f8cb8ec
nmatseke <nmatseke@finchoice.co.za>
2025-09-08 12:44:15 +0200

    fix line breaks

--- backend/server.js
+++ backend/server.js
@@ -1058,7 +1058,10 @@ if(manifestType !== 'Receipt No'){
         .fillColor('#000')
         .font('Helvetica-Bold')
         .fontSize(9)
-        .text(label + ':', margin + 5, y + 4,{continued: true});
+        .text(label + ':', margin + 5, y + 4,{
+          width: leftWidth - 75,
+          lineBreak: false
+        });
 
       doc
         .fillColor('#000')
@@ -1067,11 +1070,11 @@ if(manifestType !== 'Receipt No'){
         .text(fieldValues[i], margin + 70, y + 4, {
           width: leftWidth - 75,
           ellipsis: true,
-          continued: true,
+          lineBreak: false
         });
     });
-  }
     doc.y = startY + totalHeight + 10;
+  }
 
     doc.end();
 
